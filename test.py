6efe4c2e7c8f5f40b0e934becd9c7d6f9909105e
JustinMannar <123104067+JustinMannar@users.noreply.github.com>
2023-02-07 16:30:59 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,27 @@
+def makeMatrix (nrows, ncols, start, incr):
+    mat = []
+
+    for i in range(0, nrows, 1):
+        mat.append([])
+        for j in range(0, ncols, 1):
+            mat[i].append(start)
+            start = start + incr
+    for i in range(0, nrows, 1):
+        print(mat[i][0], mat[i][1], mat[i][2], mat[i][3], mat[i][4], mat[i][5])
+
+
+print(makeMatrix(6, 6, 1, 1))
+
+print(makeMatrix(6, 6, 2, 3))
+
+print(makeMatrix(6, 6, 0.2, 0.2))
+
+print(makeMatrix(4, 6, 10, -2))
+
+print(makeMatrix(4, 6, -6, 1.5))
+
+print(makeMatrix(2, 4, -10, 10))
+
+
+
+
