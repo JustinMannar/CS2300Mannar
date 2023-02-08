1913a79b4225cf0d50390e672b29b705fe4e29e8
JustinMannar <123104067+JustinMannar@users.noreply.github.com>
2023-02-07 17:08:02 -0700

    Add files via upload
    newest project 1

--- test.py
+++ test.py
@@ -8,6 +8,12 @@ def makeMatrix (nrows, ncols, start, incr):
             start = start + incr
     for i in range(0, nrows, 1):
         print(mat[i][0], mat[i][1], mat[i][2], mat[i][3], mat[i][4], mat[i][5])
+    return mat
+
+def writeToFile(matrix,file):
+    f=open(file,"a")
+    f.write(matrix)
+    f.close()
 
 
 print(makeMatrix(6, 6, 1, 1))
@@ -20,7 +26,8 @@ print(makeMatrix(4, 6, 10, -2))
 
 print(makeMatrix(4, 6, -6, 1.5))
 
-print(makeMatrix(2, 4, -10, 10))
+#print(makeMatrix(2,4,-10,10))
+
 
 
 
